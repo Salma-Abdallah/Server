9998d99336407928f5b1c261f6f7de7ae9543e6f
Sara Adel <saraadelaly97@gmail.com>
2023-01-27 02:31:01 +0200

    adding 2nd model class

---
+++ talkie_server/src/main/java/gov/iti/jets/models/UserGroup.java
@@ -0,0 +1,48 @@
+package gov.iti.jets.models;
+
+public class UserGroup {
+
+    private int user_group_id;
+    private int group_chat_id;
+    private int user_id;
+
+    public UserGroup(int user_id, int user_group_id, int group_chat_id) {
+        this.group_chat_id = group_chat_id;
+        this.user_id = user_id;
+        this.user_group_id = user_group_id;
+    }
+
+    /*---------------------------------------------------------------------------------*/
+    public void setUser_group_id(int user_group_id) {
+        this.user_group_id = user_group_id;
+    }
+
+    public int getUser_group_id() {
+        return user_group_id;
+    }
+
+    /*--------------------------------------------------------------------------------------*/
+    public void setGroup_chat_id(int group_chat_id) {
+        this.group_chat_id = group_chat_id;
+    }
+
+    public int getGroup_chat_id() {
+        return group_chat_id;
+    }
+
+    /*--------------------------------------------------------------------------------------------*/
+    public void setUser_id(int user_id) {
+        this.group_chat_id = group_chat_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    /*-----------------------------------------------------------------------------------------------*/
+    public String toString() {
+        return "User Group [User_group_id=" + user_group_id + ", Group_chat_id=" + group_chat_id + ", User_id ="
+                + user_id + "]";
+    }
+
+}
